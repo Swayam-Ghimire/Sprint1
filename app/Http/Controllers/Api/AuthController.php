74984e24545ad99f28f011632e9358ed616f8f16
Swayam <subinghi22@gmail.com>
2026-02-17 14:23:15 +0545

    W3Day3 - Implemented authentication using laravel sanctum

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,58 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\Api\LoginRequest;
+use App\Http\Requests\Auth\Api\RegisterRequest;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+
+        $user = User::create($data);
+        $role = Role::where('name', 'admin')->first();
+        $user->roles()->attach($role);
+        $deviceName = $request->header('User-Agent', 'Unknown Device');
+
+        return response()->json([
+            'message' => 'Registered',
+            'access_token' => $user->createToken($deviceName)->plainTextToken,
+            'user' => $user,
+            'role' => $user->roles->pluck('name'),
+        ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+        $deviceName = $request->header('User-Agent', 'Unknown Device');
+        $user = User::where('email', $data['email'])->first();
+        if (! $user || ! (Hash::check($data['password'], $user->password))) {
+            return response()->json([
+                'message' => 'The entered credentials are incorrect',
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'Logged In',
+            'access_token' => $user->createToken($deviceName)->plainTextToken,
+            'user' => $user,
+            'role' => $user->roles->pluck('name'),
+        ], 200);
+
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully',
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\CategoryController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(CategoryController::class)->group(function() {
+Route::controller(CategoryController::class)->middleware('auth:sanctum')->group(function () {
     Route::get('/categories', 'list');
     Route::get('/categories/{category}', 'show');
     Route::post('/create/category', 'create');
     Route::delete('/destroy/{category}', 'delete');
-    Route::put('/edit/{category}', 'update');   
+    Route::put('/edit/{category}', 'update');
 });
 
+// middleware admin in the future
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum');
+});
